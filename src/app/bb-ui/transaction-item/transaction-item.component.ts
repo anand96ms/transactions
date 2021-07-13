01e9f0806bac062df08030256aa89409b4026362
Anand Mohan Singh <anand.cse.imps@gmail.com>
2021-07-14 01:19:12 +0530

    reupload

--- src/app/bb-ui/transaction-item/transaction-item.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-//dirty
-@Component({
-  selector: 'app-transaction-item',
-  templateUrl: './transaction-item.component.html',
-  styleUrls: ['./transaction-item.component.scss']
-})
-export class TransactionItemComponent {}
