e9780a6b66c908c636b7c3e369fb4d54657e03d7
Anand Mohan Singh <anand.cse.imps@gmail.com>
2021-07-14 09:34:39 +0530

    Lint fix

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -1,77 +1,77 @@
 export let data = [
   {
-    "categoryCode": "#12a580",
-    "dates": {
-      "valueDate": 1600493600000
+    categoryCode: '#12a580',
+    dates: {
+      valueDate: 1600493600000
     },
-    "transaction": {
-      "amountCurrency": {
-        "amount": 5000,
-        "currencyCode": "EUR"
+    transaction: {
+      amountCurrency: {
+        amount: 5000,
+        currencyCode: 'EUR'
       },
-      "type": "Salaries",
-      "creditDebitIndicator": "CRDT"
+      type: 'Salaries',
+      creditDebitIndicator: 'CRDT'
     },
-    "merchant": {
-      "name": "Backbase",
-      "accountNumber": "SI64397745065188826"
+    merchant: {
+      name: 'Backbase',
+      accountNumber: 'SI64397745065188826'
     }
   }, {
-    "categoryCode": "#12a580",
-    "dates": {
-      "valueDate": 1600387200000
+    categoryCode: '#12a580',
+    dates: {
+      valueDate: 1600387200000
     },
-    "transaction": {
-      "amountCurrency": {
-        "amount": "82.02",
-        "currencyCode": "EUR"
+    transaction: {
+      amountCurrency: {
+        amount: '82.02',
+        currencyCode: 'EUR'
       },
-      "type": "Card Payment",
-      "creditDebitIndicator": "DBIT"
+      type: 'Card Payment',
+      creditDebitIndicator: 'DBIT'
     },
-    "merchant": {
-      "name": "The Tea Lounge",
-      "accountNumber": "SI64397745065188826"
+    merchant: {
+      name: 'The Tea Lounge',
+      accountNumber: 'SI64397745065188826'
     }
   }
 ];
 export let sortedBydateData = [
   {
-    "categoryCode": "#12a580",
-    "dates": {
-      "valueDate": 1600493600000
+    categoryCode: '#12a580',
+    dates: {
+      valueDate: 1600493600000
     },
-    "transaction": {
-      "amountCurrency": {
-        "amount": 5000,
-        "currencyCode": "EUR"
+    transaction: {
+      amountCurrency: {
+        amount: 5000,
+        currencyCode: 'EUR'
       },
-      "type": "Salaries",
-      "creditDebitIndicator": "CRDT"
+      type: 'Salaries',
+      creditDebitIndicator: 'CRDT'
     },
-    "merchant": {
-      "name": "Backbase",
-      "accountNumber": "SI64397745065188826"
+    merchant: {
+      name: 'Backbase',
+      accountNumber: 'SI64397745065188826'
     }
   }, {
-    "categoryCode": "#12a580",
-    "dates": {
-      "valueDate": 1600387200000
+    categoryCode: '#12a580',
+    dates: {
+      valueDate: 1600387200000
     },
-    "transaction": {
-      "amountCurrency": {
-        "amount": "82.02",
-        "currencyCode": "EUR"
+    transaction: {
+      amountCurrency: {
+        amount: '82.02',
+        currencyCode: 'EUR'
       },
-      "type": "Card Payment",
-      "creditDebitIndicator": "DBIT"
+      type: 'Card Payment',
+      creditDebitIndicator: 'DBIT'
     },
-    "merchant": {
-      "name": "The Tea Lounge",
-      "accountNumber": "SI64397745065188826"
+    merchant: {
+      name: 'The Tea Lounge',
+      accountNumber: 'SI64397745065188826'
     }
   }
-]
+];
 export let seachResultData = [
   {
     categoryCode: '#12a580',
